44769e8e3f812ecc7ffac3b6de0050699054d0e0
lgaxd <lucasgrilloxd@gmail.com>
2026-08-23 22:28:16 -0300

    Ajustando tests

--- src/test/java/br/com/petflow/petflow_api/PetflowApiApplicationTests.java
+++ src/test/java/br/com/petflow/petflow_api/PetflowApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.petflow.petflow_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("h2")
 class PetflowApiApplicationTests {
 
 	@Test
